4b69194fa05a0ef2ca0c019354c148518b456aad
Shivam <shivamchawla60@gmail.com>
2018-03-31 13:07:33 +0530

    live streaming with PI-CAM

--- 1.py
+++ 1.py
@@ -1,64 +1,21 @@
-from threading import Thread, Lock
-import cv2
-face_cascade = cv2.CascadeClassifier('opencv-files/haarcascade_frontalface_alt2.xml')
-def detect_face(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)       
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+from threading import Thread
+import queue
 
-    if (len(faces) == 0):
-        return None, None
-    
-    (x, y, w, h) = faces[0]
-    return gray[y:y+w, x:x+h], faces[0]
-def draw_rectangle(img, rect):
-    (x, y, w, h) = rect
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-class WebcamVideoStream :
-    def __init__(self, src = 0, width = 320, height = 240) :
-        self.stream = cv2.VideoCapture('192.168.43.106:3333')
-        self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-        self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-        (self.grabbed, self.frame) = self.stream.read()
-        self.started = False
-        self.read_lock = Lock()
+def do_stuff(q):
+  while True:
+    print (q.get())
+    q.task_done()
 
-    def start(self) :
-        if self.started :
-            print ("already started!!")
-            return None
-        self.started = True
-        self.thread = Thread(target=self.update, args=())
-        self.thread.start()
-        return self
+q = queue.Queue(maxsize=0)
+num_threads = 1
 
-    def update(self) :
-        while self.started :
-            (grabbed, frame) = self.stream.read()
-            self.read_lock.acquire()
-            self.grabbed, self.frame = grabbed, frame
-            self.read_lock.release()
+for i in range(num_threads):
+  worker = Thread(target=do_stuff, args=(q,))
+  worker.setDaemon(True)
+  worker.start()
 
-    def read(self) :
-        self.read_lock.acquire()
-        frame = self.frame.copy()
-        self.read_lock.release()
-        return frame
-
-    def stop(self) :
-        self.started = False
-        self.thread.join()
-
-    def __exit__(self, exc_type, exc_value, traceback) :
-        self.stream.release()
-
-if __name__ == "__main__" :
-    vs = WebcamVideoStream().start()
-    while True :
-        frame = vs.read()
-        #face,rect=detect_face(frame)
-#        if face is not None:
-#            draw_rectangle(frame,rect)
-        cv2.imshow('vf',frame)
-
-    vs.stop()
-    cv2.destroyAllWindows()
+for y in range (10):
+  for x in range(100):
+    q.put(x + y * 100)
+  q.join()
+  print ("Batch " + str(y) + " Done")
--- picamera-stream-server.py
+++ picamera-stream-server.py
@@ -1,8 +1,30 @@
 import io,cv2
 import socket,numpy as np
-import struct
+import struct,time
 from PIL import Image
 
+from threading import Thread
+import queue
+#video=cv2.VideoWriter('video.avi',-1,1,(width,height))
+def do_stuff(q):
+  while True:
+  	image=q.get()
+  	cv2.imshow('picam',np.array(image))
+  	cv2.waitKey(1)
+  	#print()
+  	#nparr =  np.array(image.getdata()).astype(np.float32).reshape( (image.size[0],image.size[1],3))
+#  	print(image)
+  	q.task_done()
+		
+
+q = queue.Queue(maxsize=0)
+num_threads = 1
+
+for i in range(num_threads):
+  worker = Thread(target=do_stuff, args=(q,))
+  worker.setDaemon(True)
+  worker.start()
+  
 server_socket = socket.socket()
 server_socket.bind(('169.254.50.99', 8000))
 server_socket.listen(0)
@@ -24,13 +46,13 @@ try:
         # processing on it
         image_stream.seek(0)
         image = Image.open(image_stream)
-        #nparr =  np.array(image.getdata()).astype(np.float32).reshape( (image.size[0],image.size[1],3) )
-        print(image)
+        
+        q.put(image)
 #        cv2.imshow('Live',nparr)
 #        cv2.waitKey(10)
-        print('Image is %dx%d' % image.size)
-        image.verify()
-        print('Image is verified')
+#        print('Image is %dx%d' % image.size)
+#        image.verify()
+#        print('Image is verified')
 finally:
     connection.close()
     server_socket.close()
